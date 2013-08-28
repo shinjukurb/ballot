e06b26282577fc3803e059b0cd5e06eabb811188
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2013-08-28 21:42:22 +0900

    add routes for top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Ballot::Application.routes.draw do
+  root to: 'welcome#index'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  # omniauth path
+  get '/signout' => 'sessions#destroy', as: :signout
+  get '/signin' => 'sessions#new', as: :signin
+
+
 end
