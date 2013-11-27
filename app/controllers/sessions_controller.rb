0916c0479624edf7d0f115964dcbc9fff458c4c0
GardenTree <takahiro.ueki@gmail.com>
2013-11-27 20:18:27 +0900

    Implement omniauth login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,19 +7,14 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.where(:provider => auth['provider'], 
+    user = User.where(:provider => auth['provider'],
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
 # Reset the session after successful login, per
 # 2.8 Session Fixation – Countermeasures:
 # http://guides.rubyonrails.org/security.html#session-fixation-countermeasures
     reset_session
     session[:user_id] = user.id
-    if user.email.blank?
-      redirect_to edit_user_path(user), :alert => "Please enter your email address."
-    else
-      redirect_to root_url, :notice => 'Signed in!'
-    end
-
+    redirect_to root_url, :notice => 'Signed in!'
   end
 
   def destroy
